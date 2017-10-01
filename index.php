9ca8c923a27304bc84020aeabe272962056606bf
Ranjan Kumar <ranjan.kumar@agoda.com>
2017-10-01 09:32:45 +0700

    Code cleanup

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-
-namespace FeedReader;
-
-/* * 
- *  define the site path 
- *  
- */
-$site_path = realpath(dirname(__FILE__));
-define('__SITE_PATH', $site_path);
-define('VIDEOS_PER_ROW', 4);
-
-require_once 'Loader.php';
-
-$loader = new Loader();
-$loader->load();
-
-$controller = new Controller\YoutubeVideoController(
-                new Model\YoutubeVideoCollection(),
-                new View\YoutubeView(),
-                "http://gdata.youtube.com/feeds/api/videos?max-results=50&orderby=published&author=linkinparktv");
-$controller->invoke();
+echo "<h1>Access denied!!</h1>";
 ?>
